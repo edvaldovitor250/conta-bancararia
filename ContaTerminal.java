044bc75f60d76612703c7496b0e4f913042b5e7e
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-05-03 18:40:55 -0300

    Project Finished

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("oi");
+        Scanner s1 = new Scanner(System.in);
 
+        System.out.println("Informe Seu nome: ");
+        String nomeCliente = s1.nextLine();
+
+        System.out.println("Informe Numero: ");
+        Integer numero = s1.nextInt();
+
+        System.out.println("Informe Agencia: ");
+        Integer agencia = s1.nextInt();
+
+        System.out.println("Informe saldo bancario: ");
+        Integer saldo = s1.nextInt();
+
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %d já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+        System.out.println(mensagem);
     }
 }
